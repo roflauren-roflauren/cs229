4b0adf394b3d3f39a2f294934e1fc12eeb16f3d7
roflauren-roflauren <93059080+roflauren-roflauren@users.noreply.github.com>
2023-04-20 22:52:19 -0700

    23-04-20: a1 q4d

--- ps1_starter/src/imbalanced/imbalanced.py
+++ ps1_starter/src/imbalanced/imbalanced.py
@@ -12,6 +12,33 @@ WILDCARD = 'X'
 # Ratio of class 0 to class 1
 kappa = 0.1
 
+def accuracy_report(preds, y_valid):
+    num_examples = len(y_valid)
+    binned_preds = [1 if pred >= 0.5 else 0 for pred in preds]
+    assert (len(binned_preds) == num_examples)
+    
+    # count correct predictions: 
+    overall, neg, pos = 0, 0, 0
+    for idx in range(num_examples): 
+        if binned_preds[idx] == y_valid[idx]: 
+            if binned_preds[idx] == 0: 
+                neg += 1
+            if binned_preds[idx] == 1: 
+                pos += 1
+            overall += 1
+    
+    # compute accuracies: 
+    num_neg_examples, num_pos_examples = list(y_valid).count(0), list(y_valid).count(1)
+    assert (num_neg_examples + num_pos_examples == num_examples)
+    overall_acc, neg_acc, pos_acc = overall/num_examples, neg/num_neg_examples, pos/num_pos_examples
+    balanced_acc = 0.5 * (neg_acc + pos_acc)
+    
+    # accuracies report: 
+    print(f'Overall accuracy :: {overall_acc:.2%}\n' + \
+          f'Balanced accuracy :: {balanced_acc:.2%}\n' + \
+          f'Negative class accuracy :: {neg_acc:.2%}\n' + \
+          f'Positive class accuracy :: {pos_acc:.2%}\n')
+    
 def main(train_path, validation_path, save_path):
     """Problem 2: Logistic regression for imbalanced labels.
 
@@ -46,36 +73,43 @@ def main(train_path, validation_path, save_path):
     # generate and save plot: 
     util.plot(x_valid, y_valid, model.theta, './plot_q4b.png')
     
-    # compute overall, per-class, and balanced accuracies: 
-    num_examples = len(y_valid)
-    binned_preds = [1 if pred >= 0.5 else 0 for pred in preds]
-    assert (len(binned_preds) == num_examples)
+    # compute and report overall, per-class, and balanced accuracies: 
+    print("\nUSING RAW TRAINING DATA:")
+    accuracy_report(preds, y_valid)
     
-    # count correct predictions: 
-    overall, neg, pos = 0, 0, 0
-    for idx in range(num_examples): 
-        if binned_preds[idx] == y_valid[idx]: 
-            if binned_preds[idx] == 0: 
-                neg += 1
-            if binned_preds[idx] == 1: 
-                pos += 1
-            overall += 1
-    
-    # compute accuracies: 
-    num_neg_examples, num_pos_examples = list(y_valid).count(0), list(y_valid).count(1)
-    assert (num_neg_examples + num_pos_examples == num_examples)
-    overall_acc, neg_acc, pos_acc = overall/num_examples, neg/num_neg_examples, pos/num_pos_examples
-    balanced_acc = 0.5 * (neg_acc + pos_acc)
-    
-    # accuracies report: 
-    print(f'Overall accuracy :: {overall_acc:.2%}\n' + \
-          f'Balanced accuracy :: {balanced_acc:.2%}\n' + \
-          f'Negative class accuracy :: {neg_acc:.2%}\n' + \
-          f'Positive class accuracy :: {pos_acc:.2%}')
-
     # Part (d): Upsampling minority class
     # Make sure to save predicted probabilities to output_path_upsampling using np.savetxt()
     # Repeat minority examples 1 / kappa times
+    
+    # generate augmentation data with (1 / kappa) - 1 copies of each minority (positive label) example: 
+    # NOTE: we subtract 1 because 1 copy of the data point already exists in the training dataset. 
+    x_augment, y_augment = [], []
+
+    for x, y in zip(x_train, y_train):
+        # if data point corresponds to minority class label: 
+        if y == 1: 
+            # create the necessary copies and add the values to the augmentation data 
+            for _ in range(int((1/kappa) - 1)): 
+                x_augment.append(x), y_augment.append(y)
+    
+    # add the augmentation data to the training data 
+    x_train_prime = np.append(x_train, x_augment, axis=0)
+    y_train_prime = np.append(y_train, np.array(y_augment), axis=0)
+
+    # instantiate and fit model:
+    upsample_model = LogisticRegression()
+    upsample_model.fit(x_train_prime, y_train_prime)
+    
+    # generate predictions using model and save:
+    upsample_preds = upsample_model.predict(x_valid)
+    np.savetxt(output_path_upsampling, upsample_preds)
+    
+    # generate and save plot: 
+    util.plot(x_valid, y_valid, upsample_model.theta, './plot_q4d.png')
+    
+    # compute and report overall, pre-class, and balanaced accuracies: 
+    print("\nUSING UPSAMPLED TRAINING DATA:")
+    accuracy_report(upsample_preds, y_valid)
     # *** END CODE HERE
 
 if __name__ == '__main__':
